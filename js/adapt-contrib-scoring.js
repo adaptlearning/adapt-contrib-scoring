0d220c802d5272641b4bc970f80cdd3e719ab3ff
Dan Ghost <daniel.ghost@kineo.com>
2026-01-02 16:19:54 +0000

    Update: Improve frequency of log data and information recorded (#26)
    * Update: Consolidate logs into one entry per set on `update` and include context for how the set was modified for easier debugging and better understanding of the user journey (fixes #25).
    
    * Update log score accordingly based on availability. Only include question modifiers that are within the set.
    
    * Change lifecycle events to log as info()

--- js/adapt-contrib-scoring.js
+++ js/adapt-contrib-scoring.js
@@ -4,6 +4,7 @@ import Logging from 'core/js/logging';
 import AdaptModelSet from './AdaptModelSet';
 import Passmark from './Passmark';
 import {
+  filterIntersectingHierarchy,
   getSubsetById,
   getSubsetsByType,
   getSubsetsByModelId,
@@ -87,7 +88,10 @@ class Scoring extends Backbone.Controller {
     const updateSubsets = !queuedChanges?.length
       ? this.subsets
       : [...new Set(queuedChanges.reduce((subsets, model) => subsets.concat(getSubsetsByModelId(model?.get('_id'))), []))];
-    updateSubsets.forEach(set => set.update());
+    updateSubsets.forEach(set => {
+      const changedSubsetModels = filterIntersectingHierarchy(this._queuedChanges, set.rawModels);
+      set.update(changedSubsetModels);
+    });
     this._queuedChanges = [];
     if (!updateSubsets.length) return;
     const isComplete = this.isComplete;
