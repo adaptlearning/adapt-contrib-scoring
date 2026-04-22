0ab116e0c89361b92fa185d8ece8ea7450d2d5c2
AzureAD\DanGhost <daniel.ghost@kineo.com>
2026-04-22 22:39:28 +0100

    Updated the handebars helpers.

--- js/helpers.js
+++ js/helpers.js
@@ -5,25 +5,71 @@ import {
 import {
   sum
 } from './utils/math';
+import {
+  getScaledScoreFromMinMax
+} from './utils/scoring';
+/** @typedef {import("./IntersectionSet").default} IntersectionSet */
 
 // Global handlebars helpers
 
-const helpers = {
-  scoreQuery(query, context) {
-    const modelId = context?.data?.root?._id;
-    query = query.replace('this', `#${modelId}`);
-    const sets = getSubsetsByQuery(query);
-    return sum(sets, 'score');
-  }
+/**
+ * Returns the intersected subsets of the intersection query string for the handlebars context.
+ * @see {@link ../INTERSECTION_QUERY.md INTERSECTION_QUERY}
+ * @param {string} query
+ * @param {*} context
+ * @returns {IntersectionSet[]}
+ */
+export function getSubsetsFromQueryContext(query, context) {
+  if (!context) throw Error('No context for getSubsetsFromQueryContext helper.');
+  const modelId = context?.data?.root?._id;
+  if (modelId) query = query.replace(/\bthis\b/g, modelId);
+  return getSubsetsByQuery(query);
+}
 
-  // score(context) {
-  //   if (!context) throw Error('No context for score helper.');
-  //   const root = context.data.root;
-  //   const modelId = root._id;
-  //   const model = data.findById(modelId);
-  //   const score = model.score;
-  //   return score;
-  // }
+const helpers = {
+  /**
+   * Returns the intersected subsets of the intersection query string.
+   * @see {@link ../INTERSECTION_QUERY.md INTERSECTION_QUERY}
+   * @example <ul>{{#each (subsetsQuery '[modelId=this](isScoreIncluded)')}}<li>{{{this._title}}}</li>{{/each}}</ul> - list all sets which intersect this model and are included in the total
+   * @example <ul>{{#each (subsetsQuery '[isScoreIncluded](isPassed)')}}<li>{{{this._title}}}</li>{{/each}}</ul> - list all passed sets which are included in the total
+   * @param {string} query
+   * @param {*} context
+   * @returns {IntersectionSet[]}
+   */
+  subsetsQuery(query, context) {
+    return getSubsetsFromQueryContext(query, context);
+  },
+
+  /**
+   * Returns the summed score for the intersected subsets of the intersection query string.
+   * @see {@link ../INTERSECTION_QUERY.md INTERSECTION_QUERY}
+   * @example {{{scoreQuery '#this'}}} - score for this model
+   * @example {{{scoreQuery '#this total'}}} - summed score for sets which intersect this model and are included in the total
+   * @param {string} query
+   * @param {*} context
+   * @returns {number}
+   */
+  scoreQuery(query, context) {
+    const sets = getSubsetsFromQueryContext(query, context);
+    return sum(sets, 'score');
+  },
+
+  /**
+   * Returns the scaledScore for the intersected subsets of the intersection query string.
+   * @see {@link ../INTERSECTION_QUERY.md INTERSECTION_QUERY}
+   * @example {{{scaledScoreQuery '#this'}}} - scaledScore for this model
+   * @example {{{scaledScoreQuery '#this total'}}} - scaledScore for sets which intersect this model and are included in the total
+   * @param {string} query
+   * @param {*} context
+   * @returns {number}
+   */
+  scaledScoreQuery(query, context) {
+    const sets = getSubsetsFromQueryContext(query, context);
+    const score = sum(sets, 'score');
+    const minScore = sum(sets, 'minScore');
+    const maxScore = sum(sets, 'maxScore');
+    return getScaledScoreFromMinMax(score, minScore, maxScore);
+  }
 };
 
 for (const name in helpers) {
